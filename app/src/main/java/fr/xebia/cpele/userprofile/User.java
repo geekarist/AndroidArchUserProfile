fd7d6d7e266530b20640b07a447f31d22e70a8d7
cpcp <christophe.pele@gmail.com>
2017-05-25 14:57:40 +0200

    Create User and UserProfileViewModel

---
+++ app/src/main/java/fr/xebia/cpele/userprofile/User.java
@@ -0,0 +1,4 @@
+package fr.xebia.cpele.userprofile;
+
+class User {
+}
---
+++ app/src/main/java/fr/xebia/cpele/userprofile/UserProfileViewModel.java
@@ -0,0 +1,9 @@
+package fr.xebia.cpele.userprofile;
+
+import android.arch.lifecycle.ViewModel;
+
+public class UserProfileViewModel extends ViewModel {
+
+    private String userId;
+    private User user;
+}
