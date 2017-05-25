8c603e195caa0fcfb314384de3e18a9b9adfddd5
cpcp <christophe.pele@gmail.com>
2017-05-25 17:41:32 +0200

    Pass user id as fragment argument

--- app/src/main/java/fr/xebia/cpele/userprofile/MainActivity.java
+++ app/src/main/java/fr/xebia/cpele/userprofile/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment userProfileFragment = new UserProfileFragment();
+        Fragment userProfileFragment = UserProfileFragment.newInstance("cpele");
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.main_fl_container_userprofile, userProfileFragment)
--- app/src/main/java/fr/xebia/cpele/userprofile/UserProfileFragment.java
+++ app/src/main/java/fr/xebia/cpele/userprofile/UserProfileFragment.java
@@ -10,13 +10,23 @@ import android.widget.TextView;
 
 public class UserProfileFragment extends Fragment {
 
+    public static final String KEY_USER_ID = "USER_ID";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_userprofile, container, false);
-        String userId = "cpele";
+        String userId = getArguments().getString(KEY_USER_ID);
         TextView userIdTextView = (TextView) view.findViewById(R.id.userprofile_tv_userid);
         userIdTextView.setText(getString(R.string.userprofile_greeting, userId));
         return view;
+    }
+
+    public static Fragment newInstance(String userId) {
+        UserProfileFragment userProfileFragment = new UserProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(KEY_USER_ID, userId);
+        userProfileFragment.setArguments(args);
+        return userProfileFragment;
     }
 }
