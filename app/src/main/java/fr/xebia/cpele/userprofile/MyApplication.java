1ed8a92bd02a0319486f8c0ad68fb88d22df73d8
cpele <geekarist@users.noreply.github.com>
2017-05-26 18:21:06 +0200

    Fix retrofit error and internet permissions

--- app/src/main/java/fr/xebia/cpele/userprofile/MyApplication.java
+++ app/src/main/java/fr/xebia/cpele/userprofile/MyApplication.java
@@ -3,6 +3,7 @@ package fr.xebia.cpele.userprofile;
 import android.app.Application;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MyApplication extends Application {
 
@@ -18,7 +19,10 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com").build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
         final UserProfileApi api = retrofit.create(UserProfileApi.class);
 
         mUserRepo = new UserRepository(api);
