003a7b878bbb0c382a5e5017869302d93a7a70db
cpele <geekarist@users.noreply.github.com>
2017-05-29 17:46:46 +0200

    Log HTTP messages

--- app/src/main/java/fr/xebia/cpele/userprofile/MyApplication.java
+++ app/src/main/java/fr/xebia/cpele/userprofile/MyApplication.java
@@ -1,7 +1,11 @@
 package fr.xebia.cpele.userprofile;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -19,10 +23,23 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(@NonNull final String message) {
+                Log.d(MyApplication.this.getClass().getSimpleName(), message);
+            }
+        });
+
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
+
         final UserProfileApi api = retrofit.create(UserProfileApi.class);
 
         mUserRepo = new UserRepository(api);
